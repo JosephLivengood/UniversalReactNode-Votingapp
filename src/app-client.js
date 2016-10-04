5e162e81f88fdcc66f3f9ffca1b2e9ba07ef5ba0
Joseph Livengood <joeylivengood@gmail.com>
2016-10-04 14:00:23 +0000

    Impli entry point for app

---
+++ src/app-client.js
@@ -0,0 +1,9 @@
+//entry point
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRoutes from './components/AppRoutes';
+
+window.onload = () => {
+  ReactDOM.render(<AppRoutes/>, document.getElementById('main'));
+};
