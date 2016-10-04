d5cee5f9c725755ed6b894b58b0a7ac53c020c94
Joseph Livengood <joeylivengood@gmail.com>
2016-10-04 13:39:14 +0000

    QuestionPage to be where voting occurs, now only displays options

---
+++ src/components/QuestionPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router';
+import NotFoundPage from './NotFoundPage';
+import QuestionMenu from './QuestionMenu';
+import questions from '../data/questions';
+
+export default class QuestionPage extends React.Component {
+  render() {
+    const id = this.props.params.id;
+    const question = question.filter((question) => question.id === id)[0];
+    if (!question) {
+      return <NotFoundPage/>;
+    }
+    const headerStyle = { backgroundImage: `url(/img/${question.cover})` };
+    return (
+      <div className="question-full">
+        <QuestionMenu/>
+        <div className="athlete">
+          <header style={headerStyle}/>
+        <h2 className="question">{questions.question}</h2>
+          <section className="responses">
+            <p>Choose from <strong>{questions.responses.length}</strong> responses:</p>
+            <ul>{
+              questions.responses.map((response, i) => <response key={i} {...response}/>)
+            }</ul>
+          </section>
+        </div>
+        <div className="navigateBack">
+          <Link to="/">« Back to the index</Link>
+        </div>
+      </div>
+    );
+  }
+}
