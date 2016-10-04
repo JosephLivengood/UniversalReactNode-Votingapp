3eda2021d53fe981bdc0d50d2c7ec22bb72d11d3
Joseph Livengood <joeylivengood@gmail.com>
2016-10-04 13:41:40 +0000

    Added notes reguarding the data model.

---
+++ src/data/questions.js
@@ -0,0 +1,23 @@
+//A TEMP FILE TO HOLD QUESTIONS BEFORE MONGODB IS IMPLI. Also, will not handle user answer saving.
+
+//THIS IS ****ABSOLUTELY NOT**** THE DATA MODEL WE WILL USE!, but works for basic placeholding/styling.
+
+
+const questions = [
+  {
+    'id': 'favorite-game',
+    'question': 'What is your favorite game',
+    'asker': 'Joseph Livengood',
+    'date': 'October 4, 2016',
+    'responses': [
+      { 'id': '1', 'response': 'Runescape'},
+      { 'id': '2', 'response': 'Call of Duty'},
+      { 'id': '3', 'response': 'Rocket League'}
+    ],
+  },
+  {
+    // ...and more...
+  }
+];
+
+export default questions;
